fb113cfbc1636eb154cc1632211e4aa08cecaa72
Joaquin Cavenaghi <joaquincave@outlook.com>
2023-01-21 11:57:10 -0300

    [inicio] primeros cambios y componentes

---
+++ eccomerce-app/src/components/CartWidget.js
@@ -0,0 +1,12 @@
+import logo from '../assets/logo-parfum.svg';
+function CartWidget() {
+    return (
+        <img src= {logo}
+        width="100"
+        height="40"
+        className="logo-parfum" 
+        alt="logo" />
+    );
+}
+
+export default CartWidget
---
+++ eccomerce-app/src/components/ItemListContainer.js
@@ -0,0 +1,12 @@
+
+
+
+function ItemListContainer(props) {
+  const greeting = 10
+  return (
+    <p className='text-white bg-dark'>items: {greeting} </p>
+  )
+}
+
+
+export default ItemListContainer
